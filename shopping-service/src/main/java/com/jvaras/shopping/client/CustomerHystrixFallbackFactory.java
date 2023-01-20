651410213e0ccc88ad9153b3039a44a03028496b
Your Name <you@example.com>
2023-01-20 15:30:24 -0500

    update customer

--- shopping-service/src/main/java/com/jvaras/shopping/client/CustomerHystrixFallbackFactory.java
+++ shopping-service/src/main/java/com/jvaras/shopping/client/CustomerHystrixFallbackFactory.java
@@ -1,2 +1,19 @@
-package com.jvaras.shopping.client;public class CustomerHystrixFallbackFactory {
+package com.jvaras.shopping.client;
+
+import com.jvaras.shopping.model.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerHystrixFallbackFactory implements CustomerClient {
+    @Override
+    public ResponseEntity<Customer> gerCustomer(Long id) {
+        Customer customer = Customer.builder()
+                .firstName("none")
+                .lastName("none")
+                .email("none")
+                .photoUrl("none")
+                .build();
+        return ResponseEntity.ok(customer);
+    }
 }
